c25cd6f0098d5fcb74f774db239cf7be9a2ff262
GideonMissoy <giddykips2030@gmail.com>
2023-11-24 04:40:31 -0800

    1. Install a package.

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,8 @@
 # Install flask from pip3
 
-
-package{'flask' :
-  ensure   => '2.1.0',
-  provider => 'pip3',
+exec { 'install_flask':
+  command => '/path/to/your/pip3 install Flask==2.1.0',
+  path    => ['/usr/bin', '/usr/local/bin'],
+  unless  => '/path/to/your/pip3 show Flask | grep Version',
 }
+
