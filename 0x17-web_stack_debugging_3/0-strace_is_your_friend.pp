9f45bf750b1f0c3068818b34686ef59bcab0a068
GideonMissoy <giddykips2030@gmail.com>
2024-03-05 18:38:24 -0800

    Updating files.

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,6 +0,0 @@
-# Fixes bad `phpp` extensions to `php` in the WordPress
-
-exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
-}
